4c9e2d7067da63ec4f31eef7ff56bbd51046e1e0
YavuzPYRZ <yavuzpoyraz03@gmail.com>
2022-03-31 20:47:03 +0200

    31.03 ikinci

--- src/day32_dateTime/C01_LocalDate.java
+++ src/day32_dateTime/C01_LocalDate.java
@@ -1,4 +1,15 @@
 package day32_dateTime;
 
+import java.time.LocalDate;
+
 public class C01_LocalDate {
+    public static void main(String[] args) {
+        LocalDate trh = LocalDate.now(); // objenin olusturuldugu tarihi trh ye atar/assegn eder
+
+        LocalDate baskaTrh= LocalDate.of(1988,06,9);
+        // istedigimiz yil ay ve gun de-gerlerine gore bize obje olusturulur.
+        System.out.println(trh); // 2022-03-31
+
+
+    }
 }
