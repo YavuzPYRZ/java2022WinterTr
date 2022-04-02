dccb93513c09591aed00d9d044cb914ed9a33a40
YavuzPYRZ <yavuzpoyraz03@gmail.com>
2022-04-02 18:10:18 +0200

    02.04 ilk

---
+++ src/day34_accessModifier_encapsulation/C01.java
@@ -0,0 +1,15 @@
+package day34_accessModifier_encapsulation;
+
+public class C01 {
+    private static int sayi = 10;
+    private static String str = "Java";
+
+    static  int acikSayi=20;
+    String acikString = "Hi";
+    C01(){
+
+    }
+
+    C01(int numara){}
+
+}
---
+++ src/day34_accessModifier_encapsulation/C03.java
@@ -0,0 +1,13 @@
+package day34_accessModifier_encapsulation;
+
+public class C03 {
+    public static void main(String[] args) {
+        // encapsule edecegimiz 2 veriable olsun.
+         int sayi;
+
+
+
+
+
+    }
+}
