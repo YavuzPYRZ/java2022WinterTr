94d63e765203dcc7b5affa4f707b7e99839ff2ec
YavuzPYRZ <yavuzpoyraz03@gmail.com>
2022-04-05 18:35:33 +0200

    05.04 ilk

---
+++ src/day36_inheritance/Memur.java
@@ -0,0 +1,10 @@
+package day36_inheritance;
+
+public class Memur extends Muhasebe{
+    
+    Memur memur1 = new Memur(),
+    // memur1 objesi Memur class'ının olmasına ragmen 
+    // inherit ettigi Muhasebe ve onun da Parent'i olan Personal clasındaki her datayı kullanabilir
+    
+    getMe
+}
