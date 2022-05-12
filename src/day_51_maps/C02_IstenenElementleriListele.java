e0c6152ef6a01822123362804e65322f885805bb
YavuzPYRZ <yavuzpoyraz03@gmail.com>
2022-05-12 18:57:10 +0200

    12.05 zweite

---
+++ src/day_51_maps/C02_IstenenElementleriListele.java
@@ -0,0 +1,19 @@
+package day_51_maps;
+
+import day49_maps.MapOlustur;
+
+import java.util.Map;
+
+public class C02_IstenenElementleriListele {
+    /*
+    Soru 2 ) Verilen map'te istenen programlama dilini bilen kişileri list olarak donduren bir method yazınız.
+
+
+    map ==>  {101=Ali, Can, java, 102=Veli, Yan, java,103=Ali, Yan, C}
+    istenen dil =Java
+    sonuç [Ali, Veli]
+     */
+    public static void main(String[] args) {
+        Map<Integer,String> sınıfMap =  MapOlustur.myMap();
+    }
+}
