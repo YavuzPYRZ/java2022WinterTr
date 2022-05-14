52eee20e2bd758c87a2be97c1909cd51d01cbaa9
YavuzPYRZ <yavuzpoyraz03@gmail.com>
2022-05-14 12:22:14 +0200

    14.04 dritte

--- src/day49_maps/MapOlustur.java
+++ src/day49_maps/MapOlustur.java
@@ -7,9 +7,11 @@ public class MapOlustur {
     public static Map<Integer,String> myMap(){ // herkes ulassın, statik olsun, Map'lerden oluşşun içi Int ve String olsun adı da myMap olsun
 
         Map<Integer,String> sinifListesi = new HashMap<>();
-        sinifListesi.put(101,"Ali, Can, Dev");
-        sinifListesi.put(102,"Veli, Yan, QA");
-        sinifListesi.put(103,"Ali, Yan, C");
+        sinifListesi.put(101,"Ali, Can, Dev, 1453");
+        sinifListesi.put(102,"Veli, Yan, QA, 1990");
+        sinifListesi.put(103,"Ali, Yan, C, 2000");
+        sinifListesi.put(104,"Ayse, Kan, C, 2001");
+        sinifListesi.put(105,"Fatma, Han, Java, 2002");
 return sinifListesi;
     }
 }
--- src/day52_maps/C01_EntrySet.java
+++ src/day52_maps/C01_EntrySet.java
@@ -3,13 +3,34 @@ package day52_maps;
 import day49_maps.MapOlustur;
 
 import java.util.Map;
+import java.util.Set;
 
 public class C01_EntrySet {
     public static void main(String[] args) {
         Map<Integer,String> sinifListMap =  MapOlustur.myMap();
 
-        System.out.println(sinifListMap.entrySet()); // [101=Ali, Can, Dev, 102=Veli, Yan, QA, 103=Ali, Yan, C]
-        System.out.println(sinifListMap); // {101=Ali, Can, Dev, 102=Veli, Yan, QA, 103=Ali, Yan, C}
+      //  System.out.println(sinifListMap.entrySet()); // bu collection [101=Ali, Can, Dev, 102=Veli, Yan, QA, 103=Ali, Yan, C]
+        System.out.println(sinifListMap); // bu map {101=Ali, Can, Dev, 102=Veli, Yan, QA, 103=Ali, Yan, C}
+/*
+myMap..entrySet(); method'u bize Entry'lerden oluşan bir set dondurur.
+Eger bu entry'leri manipule etmek istiyorsak
+mehthod'un dondurdugu Set'i yeni bir Set olsturup store etmeliyiz.
+ */
+        Set<Map.Entry<Integer,String>> sinifEntrySeti = sinifListMap.entrySet(); // map'i entry'lere cevirdik (entry = key+value)
 
+        System.out.println("No   İsim   Soyisim   Brans  D.yılı");
+        System.out.println("---------------------------------------");
+
+        for (Map.Entry<Integer,String> each: sinifEntrySeti
+             ) {
+            Integer entryKey = each.getKey();
+            String entryValue = each.getValue();
+
+            String valueArr[] = entryValue.split(", ");
+
+            System.out.printf( "%-4d %-6s %-9s %-6s %s\n" , entryKey , valueArr[0] , valueArr[1] ,valueArr[2] ,valueArr[3]);
+
+        }
+        System.out.println("--------------------------------------");
     }
 }
